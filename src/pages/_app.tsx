8c61dbd2596f38ac7b7243e581b8fb68eb4bae27
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-11 18:50:16 -0300

    refactor(_app.tsx): replace img tag with next/image component to improve performance and accessibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { globalStyles } from '@/styles/global'
 import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
 import logo from './../assets/Logo.svg';
+import Image from 'next/image';
 
 globalStyles();
 
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logo.src} alt="" />        
+        <Image src={logo} alt='Image' />      
       </Header>
       <Component {...pageProps} />
     </Container>
