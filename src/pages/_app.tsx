c6802fd84a04ddf656185eb6317be8cf95a2b261
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-05-11 18:21:28 -0300

    add styles page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app';
 import type { AppProps } from 'next/app'
+import logo from './../assets/Logo.svg';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <Container>
+      <Header>
+        <img src={logo.src} alt="" />        
+      </Header>
+      <Component {...pageProps} />
+    </Container>
   )
 }
